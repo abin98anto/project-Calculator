473247e923dd328a2f1ec8da8c8f383629b94058
Abin Anto <106867862+abin98anto@users.noreply.github.com>
2022-12-13 10:55:05 +0530

    step 1 complete: all arithmetic functions are addded.

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Functions");
+
+function add(num1,num2){
+    let ans=num1+num2;
+    console.log(ans);
+}
+add(2,8);
+
+function sub(num1,num2){
+    let ans=num1-num2;
+    console.log(ans);
+}
+sub(2,8);
+
+function mul(num1,num2){
+    let ans=num1*num2;
+    console.log(ans);
+}
+mul(2,8);
+
+function div(num1,num2){
+    let ans=num1/num2;
+    console.log(ans);
+}
+div(2,8);
